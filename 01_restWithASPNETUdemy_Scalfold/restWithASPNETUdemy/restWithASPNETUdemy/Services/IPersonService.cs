32556c6aaad7be5a89690e594c97467621b2d221
unknown <claudioht@gmail.com>
2022-08-30 15:40:08 -0300

    .net Commit

---
+++ 01_restWithASPNETUdemy_Scalfold/restWithASPNETUdemy/restWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using restWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace restWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long Id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicroservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext: DbContext
+    {
+       public MySQLContext(){}
+       public MySQLContext(DbContextOptions<MySQLContext> option):base(option){}
+       public DbSet<Product> Products { get; set; }
+    }
+}
